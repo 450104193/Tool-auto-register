6855fef194eeceb859cbbb59294cc29666f2f617
Phong940253 <phong940253@gmail.com>
2020-08-24 11:13:23 +0700

    fix some bug

--- test.py
+++ test.py
@@ -9,22 +9,25 @@ HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko
            'origin': URL, 'referer': URL + LOGIN_ROUTE}
 
 time_refresh_login = 1
-f = open("codeclass.txt", "r")
-username = f.readline()
-password = f.readline()
+file = open("codeclass.txt", "r")
+# Mo file cai dat
+username = file.readline()
+password = file.readline()
 
 s = requests.session()
 login_payload = {
     'username': username,
     'password': password,
 }
+# Thiet lap payload cho dang nhap
 
-error = 0
+number_of_courses_registered_failed = 0
 # Hoc phan dang ki that bai
-fall = []
-success = 0
+course_registration_failed = []
+# List cac hoc phan dang ki that bai
+number_of_courses_registered_success = 0
 # Hoc phan dang ki thanh cong
-right = []
+course_registration_success = []
 
 while True:
     login_req = s.post(URL + LOGIN_ROUTE, headers=HEADERS, data=login_payload)
@@ -36,43 +39,46 @@ while True:
     else:
         print("Chưa đến thời hạn đăng ký học phần", login_req.status_code)
 print("Đăng nhập thành công OK")
+
 cookies = login_req.cookies
+# luu cookies dang nhap
 
-code_class = f.readline()
+code_class = file.readline()
 code_class = code_class.split("|")
-type_class = f.readline()
+type_class = file.readline()
 type_class = type_class.split("|")
-register_payload = {'Hide': '', 'acceptConflict': 'false',
-                    'classStudyUnitConflictId': '', 'RegistType': ''}
 
+register_payload = {
+    'Hide': '',
+    'acceptConflict': 'false',
+    'classStudyUnitConflictId': '',
+    'RegistType': ''
+}
+# thiet lap payload cho dang ki
 
 for i in range(len(code_class)):
     temp_register_payload = register_payload
     temp_register_payload["Hide"] = code_class[i][0:14] + \
         "$0.0$" + code_class[i][0:12] + "$$0|"
-    # temp_register_payload["RegistType"] = type_class[i]
-    # print(temp_register_payload)
     while True:
         register = s.get(URL + "DangKyHocPhan/DangKy",
                          params=temp_register_payload, timeout=60)
-        # print(register.url)
         if register.status_code == 200:
             if "Đăng ký thành công" in register.text or "Lớp học phần này đã đăng ký" in register.text:
                 print("Đăng ký thành công học phần " +
                       code_class[i][0:14] + " OK")
-                success += 1
-                right.append(code_class[i])
+                number_of_courses_registered_success += 1
+                course_registration_success.append(code_class[i])
                 break
             elif "Trùng lịch:" in register.json()['Msg']:
                 temp_register_payload['acceptConflict'] = 'true'
                 temp_register_payload['classStudyUnitConflictId'] = code_class[i][0:14]
-                # temp_register_payload['RegistType'] = ''
                 print("Gặp lỗi đang thử lại!!", register.json()['Msg'])
             elif "đủ số lượng" in register.text:
                 print(
                     "Học phần đủ số lượng, chuyển qua đăng kí học phần khác!!")
-                error += 1
-                fall.append(code_class[i])
+                number_of_courses_registered_failed += 1
+                course_registration_failed.append(code_class[i])
                 break
             else:
                 print("Gặp lỗi đang thử lại!!", register.json()['Msg'])
@@ -80,12 +86,12 @@ for i in range(len(code_class)):
             print("Gặp lỗi đang thử lại", register.status_code)
         time.sleep(1)
 
-print("Đã đăng kí xong OK,", error, "học phần đã full,",
-      success, "học phần thành công")
+print("Đã đăng kí xong OK,", number_of_courses_registered_failed, "học phần đã full,",
+      number_of_courses_registered_success, "học phần thành công")
 print("Các học phần thất bại:", end=' ')
-for i in fall:
+for i in course_registration_failed:
     print(i[0:14], end=' ')
 print("")
 print("Các học phần thành công:", end=' ')
-for i in right:
+for i in course_registration_success:
     print(i[0:14], end=' ')
